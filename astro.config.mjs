8b2b3a2651bca17a21b39cda88c72b12c1249ae1
sage masumune <sage_masumune@sage-retina.local>
2026-07-07 16:06:21 +0900

    Fix base URL trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://soulwearsage.github.io',
-  base: '/mood-haus-site',
+  base: '/mood-haus-site/',
   output: 'static',
 });
